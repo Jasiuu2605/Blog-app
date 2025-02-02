ca724a73a7aacbf79321d06eb47b0023910956c7
Jan Bańczerowski <jasiuu2605@gmail.com>
2025-02-02 18:41:22 +0100

    Creating posts grid and post item components

---
+++ components/home-page/featured-posts.js
@@ -0,0 +1,10 @@
+import clsses from "./featured-posts.module.css";
+function FeaturedPosts() {
+  return (
+    <section>
+      <h2>Featured Posts</h2>
+    </section>
+  );
+}
+
+export default FeaturedPosts;
