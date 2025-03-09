12d84faefe22bcfb4e4d90c0c87a86d0bb8e3465
Jan Bańczerowski <jasiuu2605@gmail.com>
2025-03-09 13:00:03 +0100

    Creating contact api route

---
+++ pages/api/contact.js
@@ -0,0 +1,25 @@
+function handler() {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input" });
+      return;
+    }
+
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+    console.log(newMessage);
+    res.status(201).json({ message: "Success", message: newMessage });
+  }
+}
