75276a14231d767c861486d2b20a507c2b8a8e8d
Jan Bańczerowski <jasiuu2605@gmail.com>
2025-11-06 16:28:26 +0100

    adding typescript in PostItem component

--- components/posts/post-item.tsx
+++ components/posts/post-item.tsx
@@ -1,19 +1,30 @@
-import Link from "next/link";
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
 
-import classes from "./post-item.module.css";
+import classes from './post-item.module.css';
 
-function PostItem(props) {
-  const { title, image, excerpt, date, slug } = props.post;
+export type PostItemProps = {
+  post: {
+    title: string;
+    image: string;
+    excerpt: string;
+    date: string;
+    slug: string;
+  };
+};
 
-  const formattedDate = new Date(date).toLocaleDateString("en-US", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
+function PostItem({ post }: PostItemProps) {
+  const { title, image, excerpt, date, slug } = post;
+
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
   });
 
   const imagePath = `/images/posts/${slug}/${image}`;
   const linkPath = `/posts/${slug}`;
+
   return (
     <li className={classes.post}>
       <Link href={linkPath}>
