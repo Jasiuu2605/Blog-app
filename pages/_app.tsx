e4518a9fedc070dbb55d989bd3128329687536f3
Jan Bańczerowski <jasiuu2605@gmail.com>
2025-11-19 20:45:17 +0100

    migrating _app.js to TypeScript and updating tsconfig

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+import Head from 'next/head';
+
+import Layout from '../components/layout/layout';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
