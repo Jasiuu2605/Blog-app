dbf3466a9dcd30a574d08ac758d6e88e99df998d
Jan Bańczerowski <jasiuu2605@gmail.com>
2026-06-26 15:50:41 +0200

    feat: enrich posts archive header

--- components/posts/all-posts.tsx
+++ components/posts/all-posts.tsx
@@ -6,9 +6,37 @@ export type AllPostsProps = {
 };
 
 export default function AllPosts({ posts }: AllPostsProps) {
+  const featuredPosts = posts.filter((post) => post.isFeatured).length;
+  const latestPostYear = new Date(posts[0]?.date ?? '').getFullYear();
+
   return (
     <section className={classes.posts}>
-      <h1>All Posts</h1>
+      <header className={classes.header}>
+        <div>
+          <span className={classes.eyebrow}>Writing archive</span>
+          <h1>All frontend notes</h1>
+          <p>
+            A growing collection of posts about building, refactoring and
+            improving small web applications with Next.js, React and TypeScript.
+          </p>
+        </div>
+
+        <dl className={classes.stats}>
+          <div>
+            <dt>Posts</dt>
+            <dd>{posts.length}</dd>
+          </div>
+          <div>
+            <dt>Featured</dt>
+            <dd>{featuredPosts}</dd>
+          </div>
+          <div>
+            <dt>Latest</dt>
+            <dd>{latestPostYear}</dd>
+          </div>
+        </dl>
+      </header>
+
       <PostsGrid posts={posts} />
     </section>
   );
